c46e6fec34d3388aec1b44d3db50107db6c50c9e
Davi <davi.gonzales.antunes@gmail.com>
2020-08-13 22:55:21 -0300

    comitando projeto ByteBank

---
+++ ContaCorrente.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class ContaCorrente
+    {
+        public static int TotalDeContasCriadas { get; set; }
+        public Cliente Titular { get; set; }
+        public int Agencia { get; set; }
+        public int Numero { get; set; }
+
+        private double _saldo;
+
+        public ContaCorrente(int agencia, int conta)
+        {
+            Agencia = agencia;
+            Numero = conta;
+
+            TotalDeContasCriadas++;
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public string Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return "Saldo insuficiente!";
+            }
+            else
+            {
+                _saldo -= valor;
+                return "Saque realizado com sucesso!";
+            }   
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public string Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return "Saldo insufuciente!";
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return "Transferencia efetuado com sucesso!";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(123, 12345678);
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Davi";
+            cliente.CPF = "452.846.388-16";
+            cliente.Profissao = "Desevolvedor .NET";
+
+            conta.Saldo = 500;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine($"Agencia: {conta.Agencia}");
+            Console.WriteLine($"Conta: {conta.Numero}");
+            Console.WriteLine($"Saldo: {conta.Saldo}");
+            Console.WriteLine($"Total de contas do banco ByteBank: {ContaCorrente.TotalDeContasCriadas}");
+
+            Console.ReadLine();
+        }
+    }
+}
